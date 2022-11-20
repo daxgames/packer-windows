e0236c023c2bb77ec765ba3f04d171fc58f8ed0e
Dax T. Games <dtgames@kinggeek.org>
2022-11-20 11:12:06 -0500

    personalize

---
+++ scripts/chocopacks.ps1
@@ -0,0 +1,3 @@
+choco install -y notepadplusplus
+choco install -y boxstarter.winconfig
+choco install -y visualstudio2022community --execution-timeout 9000 --package-parameters "--add Microsoft.VisualStudio.Workload.NativeDesktop;includeRecommended"
