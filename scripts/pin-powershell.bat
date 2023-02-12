f74d48469e6b9fb81ae88b1e34195a590752d5e8
Dax T. Games <dtgames@kinggeek.org>
2023-02-12 18:29:53 -0500

    fixes

--- scripts/pin-powershell.bat
+++ scripts/pin-powershell.bat
@@ -1,14 +1,14 @@
 rem https://connect.microsoft.com/PowerShell/feedback/details/1609288/pin-to-taskbar-no-longer-working-in-windows-10
 
 set ps_link=A:\WindowsPowerShell.lnk
-if exist e:\WindowsPowerShell.lnk {
+if exist e:\WindowsPowerShell.lnk ()
   set ps_link=e:\WindowsPowerShell.lnk
-}
+)
 
 set pin_to_10=A:\PinTo10.exe
-if exist e:\PinTo10.exe {
+if exist e:\PinTo10.exe (
   set pin_to_10=e:\PinTo10.exe
-}
+)
 
 
 copy "%ps_link%" "%TEMP%\Windows PowerShell.lnk"
